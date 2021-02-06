101953b40829981a67bb244a0dedfce4907243a6
littlelogic <littlelogic@foxmail.com>
2021-02-06 18:51:58 +0800

    添加few方法

--- utils/src/main/java/com/badlogic/utils/Tools.java
+++ utils/src/main/java/com/badlogic/utils/Tools.java
@@ -108,7 +108,62 @@ import java.util.regex.Pattern;
 public class Tools {
 
 
+    /**
+     * 获取文件夹大小
+     * @param file File实例
+     * @return long
+     */
+    public static long getFolderSize(File file) {
+        long size = 0;
+        try {
+            File[] fileList = file.listFiles();
+            for (int i = 0; i < fileList.length; i++) {
+                if (fileList[i].isDirectory()) size = size + getFolderSize(fileList[i]);
+                else size = size + fileList[i].length();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return size;
+    }
 
+    public static long getFolderSize(String filePath) {
+        long size = 0;
+        try {
+            File file = new File(filePath);
+            return getFolderSize(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return size;
+    }
+
+    /**
+     * 删除指定目录下文件及目录
+     * @param deleteThisPath
+     * @return
+     */
+    public static  void deleteFolderFile(String filePath, boolean deleteThisPath) {
+        if (!TextUtils.isEmpty(filePath)) {
+            try {
+                File file = new File(filePath);
+                if (file.isDirectory()) {// 处理目录
+                    File files[] = file.listFiles();
+                    for (int i = 0; i < files.length; i++) {
+                        deleteFolderFile(files[i].getAbsolutePath(), true);
+                    }
+                }
+                if (deleteThisPath) {
+                    if (!file.isDirectory()) {// 如果是文件，删除
+                        file.delete();
+                    } else {// 目录
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     ///=========================
 
